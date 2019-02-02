182de4c2de675160673b8a7b1d0f2cd31f06a915
ugo510 <ummagohil@gmail.com>
2019-02-02 14:27:51 +0000

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Home from "../components/home"
+import About from "../components/about"
+import Blog from "../components/blog"
+import Contact from "../components/contact"
+import Portfolio from "../components/portfolio"
+import Footer from "../components/footer"
+
+export default () => (
+    <div>
+      <Home />
+      <About />
+      <Blog />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  )
