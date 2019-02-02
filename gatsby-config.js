0f268ccea672cdcbabf5096cb2123faee575d665
ugo510 <ummagohil@gmail.com>
2019-02-02 14:54:31 +0000

    Emotion Plug-in

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+      {
+        resolve: `gatsby-plugin-emotion`,
+        options: {
+          // Accepts all options defined by `babel-plugin-emotion` plugin.
+        },
+      },
+    ],
+  }
