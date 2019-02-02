d673c9850d84497afccbbd2f5a199b510f11afdc
ugo510 <ummagohil@gmail.com>
2019-02-02 14:56:14 +0000

    Config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    siteMetadata: {
+      title: `Umma Gohil`,
+      siteUrl: `https://www.ummagohil.com`,
+      description: `Creative Developer in London`,
+    },
+  }
+
+module.exports = {
     plugins: [
       {
         resolve: `gatsby-plugin-emotion`,
